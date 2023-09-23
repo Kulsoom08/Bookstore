2c078fe78b0867ee9b8d993a85761f34483bb196
Kulsoom08 <120732404+Kulsoom08@users.noreply.github.com>
2023-09-23 21:40:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import SearchBar from './SearchBar.js';
+import './style.css';
+import './App.css';
+
+
+
+
+function App() {
+  return (
+   <div>
+      <SearchBar/>
+      
+   </div>
+
+  
+  )
+}
+
+export default App;
