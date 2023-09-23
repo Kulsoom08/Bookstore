effb0d596daa9c32ee6343529b0bde7e1eac6b30
Kulsoom@1996 <kulsoomfatima896@gmail.com>
2023-09-23 21:35:35 +0530

    Initial commit

---
+++ src/Navbar.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import { BsFillBellFill } from "react-icons/bs";
+function Navbar() {
+  return (
+   <>
+      <div>
+         <h4>KeazoNBooks</h4>
+           <div>
+            <input type='text' placeholder='Search for the Books you want and read it now...Sherlock Holmes,Harry Pot...'></input>
+            <button>Search</button>
+         </div>
+         <div>
+
+         </div>  
+
+      </div>
+
+   </>
+  )
+}
+
+export default Navbar;
